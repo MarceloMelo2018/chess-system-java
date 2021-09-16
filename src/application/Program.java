4c8d86bba3a7ac1352275c0b36e62959ea008357
Marcelo Melo <marcelomelo.berto@hotmail.com>
2021-09-16 15:05:52 -0300

    Rook class update and Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,12 +19,15 @@ public class Program {
 			
 			try {
 				UI.clearScreen();
-				System.out.println();
 				UI.printBoard(chessMatch.getPieces());
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
