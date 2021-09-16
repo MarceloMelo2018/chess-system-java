72f4010e5674b5224df7e741e4b6c3e9d8174a91
Marcelo Melo <marcelomelo.berto@hotmail.com>
2021-09-16 19:18:12 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -47,7 +47,7 @@ public class UI {
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
 		} catch (RuntimeException e) {
-			throw new InputMismatchException("Error reading ChessPosition: Valid values are from a1 to h8");
+			throw new InputMismatchException("Error reading ChessPosition: Valid values are from a1 to h8.");
 		}
 	}
 
@@ -56,10 +56,15 @@ public class UI {
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
-		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		System.out.println("Turn : " + chessMatch.getTurn());
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		} else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
